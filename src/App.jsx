e62cd147732e4e36f5d413128c1543e33233a2ae
Emelinur <emelinuryzgc@gmail.com>
2024-07-30 17:53:00 +0300

    App.css dosyası düzenlendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
 import "./App.css";
-import { createGlobalStyle } from "styled-components";
 import { Navbar } from "./components/Navbar";
 import { SideBar } from "./components/Sidebar";
-const GlobalStyle = createGlobalStyle`
-  body {
-    background-color: #F4F7FD;
-    width: 1140px;
-    height: 1024px;
-    margin: 0;
-    padding: 0;
-  font-family: "Plus Jakarta Sans", sans-serif;
-  font-optical-sizing: auto;
-  font-style: normal;
 
-  }
-`;
 
 function App() {
   return (
     <>
-      <GlobalStyle />
       <Navbar />
       {/* <SideBar /> */}
     </>
