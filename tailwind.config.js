fcf3d7088a877080042a293d6befdedcd0235508
Emel <73778667+Emelinur@users.noreply.github.com>
2024-08-26 00:15:38 +0300

    "index.html,app.css,BoardNavbar,tailwind.config dosyası güncellendi"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-   
     extend: {
-      colors:{
-        lightGrey:"#E4EBFA",
-        mediumGrey:"#828FA3",
-        mainPurple:"#635FC7",
-       border:"#E4EBFA",
-       mediumGrey:"#828FA3"
-
+      colors: {
+        black: "#000112",
+        veryDarkGreyBG:"#20212C",
+        darkGrey: "#2B2C37",
+        linesDark:"#3E3F4E",
+        mediumGrey: "#828FA3",
+        linesLight: "#E4EBFA",
+        lightGreyBG: "#F4F7FD",
+        mainPurple: "#635FC7",
+        mainPurpleHover: "#A8A4FF",
+        red:"#EA5555",
+        redHover:"#FF9898",
+        border: "#E4EBFA",
       },
+    width:{
+      "300":"18.75rem",
+      "276":"17.25rem",
+      "251":"15.688rem",
+    },
+
     },
   },
   plugins: [],
-}
+};
